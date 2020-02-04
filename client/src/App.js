647c0b592c495fe220ade3e5299f79f779b48d4d
Phil Swartz <pswartz.sju@gmail.com>
2020-02-04 00:21:15 -0500

    trying to blur background image

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="bg-image">
       <Router>
         <div>
           <Switch>
@@ -13,6 +14,7 @@ function App() {
           </Switch>
         </div>
       </Router>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,6 @@ class Home extends Component {
     render = () => {
         return (
             <div>
-
                 <Link 
                     to="/Home"
                     className={window.location.pathname === "/Home"}
