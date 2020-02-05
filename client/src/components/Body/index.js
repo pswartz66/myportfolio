851a87efef3f9128b7b79324ea7dcf6df01e475e
Phil Swartz <pswartz.sju@gmail.com>
2020-02-04 22:46:17 -0500

    made some wording changes in intro

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -26,16 +26,16 @@ class Body extends Component {
                 <div id="section2" className="bodyContainer">
                     <Row>
                         <Col className="col-12">
-                            <h2 className="d-flex justify-content-start p-4">Phil Swartz</h2>
+                            <h2 className="d-flex justify-content-start">Phil Swartz</h2>
                             <div>
                                 <p className="intro">
                                     Phil recently graduated from the Penn LPS coding bootcamp.
                                     There he learned how to build <span className="strong-text">full stack</span> websites using languages like
                                     <span className="strong-text"> javascript</span>, html, and css in conjunction with <span className="strong-text"> nodeJS</span>, mySQL, and mongoDB.
                                     In addition he spent time creating <span className="strong-text"> React</span> apps, learning multiple api's,
-                                    and handled routing <span className="strong-text">data</span> via client server using <span className="strong-text">Express</span> middleware. In
-                                    his free time he likes to ride his bike through the streets of <span className="strong-text">Philadelphia</span>,
-                                    make cool videos with his GoPro, and produce music.
+                                    and routing <span className="strong-text">data</span> via client server using <span className="strong-text">Express</span> middleware. 
+                                    Majority of his apps were built following the <span className="strong-text">MVC paradigm</span>. In his free time he likes to ride his bike through 
+                                    the streets of <span className="strong-text">Philadelphia</span>, make cool videos with his GoPro, and produce music.
                                 </p>
                             </div>
                         </Col>
